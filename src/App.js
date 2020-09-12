847daa50f666154e13a0ae87b2893930eee9e946
Tran The Hai <haitt060699@gmail.com>
2020-09-12 21:07:25 +0700

    modified

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
             <nav>
               <ul>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link to="/">Homes</Link>
                 </li>
                 <li>
                   <Link to="/photos">Photos</Link>
